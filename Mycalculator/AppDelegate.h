72569359abde9119ca7acbe85ecf207ecc00959f
gurpreet <gurpreet@gurpreets-MacBook-Pro.local>
2015-09-20 01:08:04 +0530

    Initial Commit

---
+++ Mycalculator/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Mycalculator
+//
+//  Created by gurpreet on 20/09/15.
+//  Copyright (c) 2015 gurpreet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
