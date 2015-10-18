1030dcb3ff850e3ac7fb06ae9f2e6d16be5104d8
gsingh1989 <singh.india1989@gmail.com>
2015-10-18 16:26:26 +0530

    test commit

---
+++ Mycalculator/calculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  calculatorBrain.h
+//  Mycalculator
+//
+//  Created by gurpreet on 20/09/15.
+//  Copyright (c) 2015 gurpreet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface calculatorBrain : NSObject
+
+@end
---
+++ Mycalculator/calculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  calculatorBrain.m
+//  Mycalculator
+//
+//  Created by gurpreet on 20/09/15.
+//  Copyright (c) 2015 gurpreet. All rights reserved.
+//
+
+#import "calculatorBrain.h"
+
+@implementation calculatorBrain
+
+@end
